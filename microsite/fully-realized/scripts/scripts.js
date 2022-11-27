7e8555cd65ccbfbb9190d837d4856919bc135703
Crossml121 <112566478+Crossml121@users.noreply.github.com>
2022-11-27 13:25:10 -0600

    Add files via upload

---
+++ microsite/fully-realized/scripts/scripts.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {			
+			    
+				$(".hamburger").click(function(){		
+					$(".navul").toggleClass("open");	
+					console.log("Toggled the class!");
+				});
+
+			});
+    
